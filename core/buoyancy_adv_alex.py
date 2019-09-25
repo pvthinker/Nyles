20b96a69cb1f74e1c48861ffa8abbab349fe7afe
Alexandre LHer <alexandre.lher@hotmail.com>
2019-09-25 14:43:03 +0200

    Advection of the buoyancy
    A function to advect the buoyancy using the contravariant velocity and 
    the volume. It uses the fortran_upwind function.
    It was coded using the same model as bernoulli.py while I started to 
    understand how everything works together.
    
    NOT TESTED YET !
    The volume is not right I think, I'm not sure that it's good to 
    duplicate the buoyancy array to store the delta buoyancy out of the 
    fortran routine.

---
+++ core/buoyancy_adv_alex.py
@@ -0,0 +1,35 @@
+"""
+Just a try while I understand how everything works together
+
+Advection of the buoyancy
+
+vol \partial_t\ b = - \partial_i\ (b vol U^i)
+
+"""
+
+import fortran_upwind as fortran
+
+def buoyancyAdv(state) :
+    """
+        Advection of the buoyancy, following the model of bernoulli.py
+
+    """
+
+    b = state.get('b')
+    U = state.get('U')
+
+    """
+        Should be dx, dy, dz to calculate the volume. Not sure from where I can get them ??
+    """
+    vol = b.nx * b.ny * b.nz
+
+    #preallocate the delta buoyancy, is it the right way ?
+    db = b.duplicate()
+
+    for d in 'ijk':
+        comp = getattr(U, d) #U.i , U.j, U.k
+
+        #upwind(trac, u, dtrac, vol, iflag, l, m, n) what is iflag ??
+        fortran.upwind(b.view(d), comp.view(d), db.view(d), vol, b.nx, b.ny, b.nz)
+
+    b += db

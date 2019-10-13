2b6761d58932716072ff4711972f4a3567086b11
Alexandre LHer <alexandre.lher@hotmail.com>
2019-10-13 20:15:36 +0200

    This commit corrects (I hope) most of the issues found by the team with my previous commit.
    fortran_vortex_force.f90 should now calculate correctly the cross 
    product between the the vorticity (by upwinding it) and the 
    contravariant velocity (by interpolating it at u-points), and substract 
    it from the rhs.
    
    vortex_force.py now has the new naming conventions. I also added in main 
    a small test of the cross product between contravariant velocity and 
    vorticity. The small series of tests realized until this commit seem to 
    work but where very limited. More exhaustive tests should be added in a 
    later commit.
    
    TODO : make vortex_force.py call the fortran routine only 3 times 
    instead of 6

--- core/fortran_vortex_force.f90
+++ core/fortran_vortex_force.f90
@@ -1,10 +1,10 @@
 !-----------------------------------------
 
-subroutine vortex_force_calc(U, vorticity, res, vol, l, m, n)
+subroutine vortex_force_calc(U, vorticity, res, epsilon ,vol, l, m, n)
 
   implicit none
 
-  integer, intent(in):: l, m, n
+  integer, intent(in):: l, m, n, epsilon
   real*8, intent(in) :: vol
   real*8, dimension(l, m, n), intent(in) :: U, vorticity
   real*8, dimension(l, m, n), intent(inout) :: res
@@ -21,51 +21,50 @@ subroutine vortex_force_calc(U, vorticity, res, vol, l, m, n)
 
   do k = 1,l
     do j = 1,m
-
-      fx = 0
-
       do i = 1,n
 
-        UU = abs(U(k,j,i))
-        up = 0.5*(U(k,j,i)+UU) ! right-going flux
-        um = 0.5*(U(k,j,i)-UU) ! left-going flux
+        if (j.gt.1) then
 
-        if (i.gt.1) then
-           ! 3rd order upwind interpolation at U point
-           qp = c1*vorticity(k,j,i-1)+c2*vorticity(k,j,i  )+c3*vorticity(k,j,i+1)
+          if (i.lt.n) then
+            U_interp = 0.25 * ( U(k, j-1, i) + U(k, j-1, i+1) + U(k, j, i) + U(k, j, i+1) )
+          else
+            U_interp = 0.25 * ( U(k, j-1, i) + U(k, j, i)) !no normal velocity
+          endif
+
+          UU = abs(U_interp)
+          up = 0.5*(U_interp+UU) ! right-going flux
+          um = 0.5*(U_interp-UU) ! left-going flux
+
+          if (i.gt.1) then
+             ! 3rd order upwind interpolation at U point
+             qp = c1*vorticity(k,j,i-1) + c2*vorticity(k,j,i) + c3*vorticity(k,j,i+1)
+          else
+             ! 1st order
+             qp = vorticity(k,j,i)
+          endif
+
+          if (i.le.(n-2)) then
+             ! 3rd order upwind
+             qm = c3*vorticity(k,j,i) + c2*vorticity(k,j,i+1) + c1*vorticity(k,j,i+2)
+          elseif (i.le.(n-1)) then
+             ! 1st order
+             qm = vorticity(k,j,i+1)
+          else
+             ! no flux through the boundary
+             ! TODO: change this in case of inflow/outflow
+             ! cf Winters 2012
+             qm = 0.
+          endif
+
+          vorticity_interp = vol*(abs(up)*qp + abs(um)*qm)
+
+          res(k,j,i) = res(k,j,i) - epsilon * U_interp * vorticity_interp
+
         else
-           ! 1st order
-           qp = vorticity(k,j,i)
+
+          U_interp = 0 !no slip BC => do nothing
+
         endif
-
-        if (i.le.(n-2)) then
-           ! 3rd order upwind
-           qm = c3*vorticity(k,j,i )+c2*vorticity(k,j,i+1)+c1*vorticity(k,j,i+2)
-        elseif (i.le.(n-1)) then
-           ! 1st order
-           qm = vorticity(k,j,i+1)
-        else
-           ! no flux through the boundary
-           ! TODO: change this in case of inflow/outflow
-           ! cf Winters 2012
-           qm = 0.
-        endif
-
-        fx = vol*(up*qp + um*qm)
-
-        vorticity_interp = fxm - fx
-
-        if ((i.gt.1) .and. (j.gt.1) .and. (i.lt.n) .and. (j.lt.m)) then
-          U_interp = 0.25 * ( U(k, j+1, i+1) + U(k, j-1, i+1) + U(k, j+1, i-1) + U(k, j-1, i-1))
-          res(k,j,i) = res(k,j,i) - U_interp * vorticity_interp
-        else
-          U_interp = 0 !BC
-          !result(k,j,i) = result(k,j,i) do nothing
-        endif
-
-
-
-        fxm = fx
 
       end do
     end do
--- core/vortex_force.py
+++ core/vortex_force.py
@@ -20,40 +20,108 @@ def vortex_force(state, rhs):
     The inner index of all the arrays is taken to be the index of the direction of upwinding,
     this should make the computation more efficient in fortran.
 
+    for example :
+        d/dt (u_i) = - ( eps_ijk * w_j * U^k + eps_ikj * w_k * U^j )
+
+        Lets consider only the first term : eps_ijk * w_j * U^k
+
+        U^k  is in the k direction, so that the upwinding of vorticity will be done also
+        in the k direction.
+        This way, we need the k direction to be the inner index of the array, they will
+        then be in the form :
+        U(j,i,k)
+        vort(j,i,k)
+        u(j,i,k)
+        And the interpolation of the velocity will need to be in k and i
+
+        In the case of the second term, the upwinding will be in U^j, so the inner index
+        will be the j direction. The arrays will then be in the form :
+        U(i,k,j)
+        vort(i,k,j)
+        u(i,k,j)
+        And the interpolation of the velocity will need to be in k and i
+
+
     The fortran subroutine imposes 0 velocity at the boudaries.
+
 
     TODO : make the function call the fortran subroutine only 3 times.
     """
 
-    positive_triplets = {"ijk","jki","kij"}
-    negative_triplets = {"kji","ikj","jik"}
+    positive_triplets = ["ijk","jki","kij"]
+    negative_triplets = ["kji","ikj","jik"]
 
 
-    for triplet in positive_triplets :
+    for i,j,k in positive_triplets :
 
         #Using the convention of taking the inner index as the index of the upwinding of vorticity
-        covariant_component = rhs.u[triplet[0]].view(triplet[2])
-        contravariant_component = state.U[triplet[2]].view(triplet[2])
-        vorticity_component = state.vor[triplet[1]].view(triplet[2])
+        u_comp = rhs.u[i].view(k)
+        U_comp = state.U[k].view(k)
+        w_comp = state.vor[j].view(k)
 
-        fortran.vortex_force_calc(contravariant_component, vorticity_component, covariant_component, 1.)
+        fortran.vortex_force_calc(U_comp, w_comp, u_comp, 1, 1.)
 
-    for triplet in negative_triplets :
+    for i,j,k in negative_triplets :
 
-        covariant_component = rhs.u[triplet[0]].view(triplet[2])
-        contravariant_component = state.U[triplet[2]].view(triplet[2]) #3rd component as inner
-        vorticity_component = state.vor[triplet[1]].view(triplet[2])
+        u_comp = rhs.u[i].view(k)
+        U_comp = state.U[k].view(k)
+        w_comp = state.vor[j].view(k)
 
-        fortran.vortex_force_calc(-contravariant_component, vorticity_component, covariant_component, 1.)
+        fortran.vortex_force_calc(U_comp, w_comp, u_comp, -1, 1.)
 
 
 
 if __name__ == '__main__':
 
     import variables as var
+    import vorticity
+    import matplotlib.pyplot as plt
+    import numpy as np
 
-    param = {'nx': 40, 'ny': 50, 'nz': 60, 'nh': 2}
+    #Verify that the cross product works
+
+    nx = 40
+    ny = 50
+    nz = 60
+    nh = 2
+
+    param = {'nx': nx, 'ny': ny, 'nz': nz, 'nh': nh}
     state = var.get_state(param)
+
+    Ui = state.U['i'].view()
+    Uj = state.U['j'].view()
+    Uk = state.U['k'].view()
+
+    Uk[:,:,:] = -1
+
+    wi = state.vor['i'].view()
+    wj = state.vor['j'].view()
+    wk = state.vor['k'].view()
+
+    wj[:,:,:] = 2
+
     ds = state.duplicate_prognostic_variables()
 
     vortex_force(state, ds)
+
+    ui = ds.u['i'].view()
+    uj = ds.u['j'].view()
+    uk = ds.u['k'].view()
+
+
+    plt.figure()
+    plt.title('ui')
+    plt.pcolor(ui[10,:,:])
+    plt.colorbar()
+
+    plt.figure()
+    plt.title('uj')
+    plt.pcolor(uj[10,:,:])
+    plt.colorbar()
+
+    plt.figure()
+    plt.title('uk')
+    plt.pcolor(uk[10,:,:])
+    plt.colorbar()
+
+    plt.show()

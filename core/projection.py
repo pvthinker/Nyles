baaf10e27202ca255382ede42c651deeb9db91b1
pvthinker <roullet@univ-brest.fr>
2019-11-01 14:00:15 +0100

    Fix projection.py
    Use covariant components for the computation of the divergence and for
    the correction.
    Todo: implement the metric tensor more cleanly

--- core/projection.py
+++ core/projection.py
@@ -16,15 +16,19 @@ def calculate_p_from_dU(mg, state, dstate):
     """
 
     # compute divergence
+    # cff is the inverse metric tensor
+    # TODO: handle this information more neatly
+    cff = {'i': 1., 'j': 1., 'k': 1.}
     for count, i in enumerate('jki'):
         div = state.work.view(i)
-        dU = dstate.U[i].view(i)
-        if count == 0: div *= 0
+        dU = dstate.u[i].view(i)*cff[i]
+        if count == 0:
+            div *= 0
         div[:, :, 1:] += dU[:, :, 1:]-dU[:, :, :-1]
 
     # at the end of the loop div and dU are in the 'i' convention
     # this is mandatory because MG only works with the 'i' convention
-        
+
     # copy divergence into the multigrid RHS
     # watch out, halo in MG is nh=1, it's wider for div
     b = mg.grid[0].toarray('b')
@@ -39,14 +43,14 @@ def calculate_p_from_dU(mg, state, dstate):
 
     # solve
     mg.solve_directly()
-    
+
     # copy MG solution to pressure
     p = state.p.view('i')
     p[mg_idx] = x[:]
 
-    # correct dU
+    # correct du (the covariant component)
     # now we start with the 'i' convention
     for i in 'ijk':
         p = state.p.view(i)
-        dU = dstate.U[i].view(i)
-        dU[:, :, :-1] -= p[:, :, 1:]-p[:, :, :-1]
+        du = dstate.u[i].view(i)
+        du[:, :, :-1] -= p[:, :, 1:]-p[:, :, :-1]

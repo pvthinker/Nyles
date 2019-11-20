9bf052fde5ee9d65220d55c9c4121cc2208cc782
Markus Reinert <markus-reinert@web.de>
2019-11-20 09:46:25 +0100

    Fix vortex experiment

--- experiments/vortices/vortices.py
+++ experiments/vortices/vortices.py
@@ -1,4 +1,4 @@
-"""Experiments with 2D vortices to check that Nyles works consistently."""
+"""Experiments with quasi 2D vortices to check that Nyles works consistently."""
 
 # Standard library imports
 from enum import Enum
@@ -16,14 +16,16 @@ class ET(Enum):
     The value of each entry of this enum is a short identifier that is
     used in the experiment name.
     """
-    Vor_const = "w_const"  # rigid body rotation
-    Vor_linear = "w_lin"  # vortex with (limited) linear vorticity profile
-    Vor_Gauss = "w_norm"  # vortex with Gauss shaped vorticity profile
-    Dipole = "dipole"  # two vortices of opposite sign
-    TwoVortices = "2_vors"  # two vortices of same sign
-    VerticalDipole = "dipole_z"  # a dipole with rotation axes parallel to x
-    VerticalTwoVortices = "2_vors_z"  # two vortices of same sign with
-                                      # rotation axes parallel to x
+    # Rotation around the z-axis (vorticity only in k-direction)
+    Vor_const = "w_const"  # constant vorticity profile: rigid body rotation
+    Vor_linear = "w_lin"  # linear vorticity profile
+    Vor_Gauss = "w_norm"  # Gauss-shaped vorticity profile
+    Dipole = "dipole"  # two vortices of opposite sign in the horizontal plane
+    TwoVortices = "2_vors"  # two vortices of the same sign
+    # Rotation around the x-axis (vorticity only in i-direction)
+    VerticalVor_const = "w_const_z"  # constant vorticity profile
+    VerticalDipole = "dipole_z"  # two vortices of opposite sign in y,z-plane
+    VerticalTwoVortices = "2_vors_z"  # two vortices of same sign
 
 
 # Select a type of experiment
@@ -73,53 +75,68 @@ nyles = Nyles(param)
 model = nyles.model
 grid = nyles.grid
 
-# Get access to the important model variables
-xr, yr, zr = grid.xr, grid.yr, grid.zr
-vor = model.state.vor
+# Get access to the coordinates at vorticity point
+# Note that the coordinates are of Scalar type
+x = grid.x_vor.view("i")
+y = grid.y_vor.view("i")
+z = grid.z_vor.view("i")
+# Get access to the three components of the discretized vorticity
+wi = model.state.vor["i"].view("i")
+wj = model.state.vor["j"].view("i")
+wk = model.state.vor["i"].view("i")
 
 ### Set the initial state of the simulation
 ## Define the radius of the vortices
 R = min([Lx, Ly]) / 2
+R_yz = min([Ly, Lz]) / 2
 ## Define the radial coordinate r of polar coordinates (r, phi) = (x, y)
-# 1) around the origin (0, 0)
-r = np.sqrt(xr**2 + yr**2)
-# 2) around (Lx/2, 0)
-r1 = np.sqrt((xr-Lx/2)**2 + yr**2)
-# 3) around (-Lx/2, 0)
-r2 = np.sqrt((xr+Lx/2)**2 + yr**2)
+# 1) around the center (Lx/2, Ly/2)
+r = np.sqrt((x-Lx/2)**2 + (y-Ly/2)**2)
+# 2) shifted to the right by 1/4 of the domain length
+r_right = np.sqrt((x-3*Lx/4)**2 + (y-Ly/2)**2)
+# 3) shifted to the left by 1/4 of the domain length
+r_left = np.sqrt((x-Lx/4)**2 + (y-Ly/2)**2)
 ## Define vertical polar coordinates (r, phi) = (y, z)
-r3 = np.sqrt(yr**2 + (zr-Lz/2)**2)
-r4 = np.sqrt(yr**2 + (zr+Lz/2)**2)
+r_yz = np.sqrt((y-Ly/2)**2 + (z-Lz/2)**2)
+r_up = np.sqrt((y-Ly/2)**2 + (z-3*Lz/4)**2)
+r_down = np.sqrt((y-Ly/2)**2 + (z-Lz/4)**2)
 ## Define the initial vorticity field
 if exp_type == ET.Vor_const:
-    vor[...] = 1
-    vor[r > R] = 0
+    wk[...] = 1
+    wk[r > R] = 0
 elif exp_type == ET.Vor_linear:
-    vor[...] = r/R
-    vor[r > R] = 0
+    wk[...] = r/R
+    wk[r > R] = 0
 elif exp_type == ET.Vor_Gauss:
-    vor[...] = np.exp(-(r/R)**2)
+    wk[...] = np.exp(-(r/R)**2)
 elif exp_type == ET.Dipole:
-    vor[...] = np.exp(-(r1/R)**2)
-    vor -= np.exp(-(r2/R)**2)
+    wk[...] = np.exp(-(r_right/R)**2)
+    wk -= np.exp(-(r_left/R)**2)
 elif exp_type == ET.TwoVortices:
-    vor[...] = np.exp(-(r1/R)**2)
-    vor += np.exp(-(r2/R)**2)
+    wk[...] = np.exp(-(r_right/R)**2)
+    wk += np.exp(-(r_left/R)**2)
+elif exp_type == ET.VerticalVor_const:
+    wi[...] = 1
+    wi[r_yz > R_yz] = 0
 elif exp_type == ET.VerticalDipole:
-    vor[...] = np.exp(-(r3/R)**2)
-    vor -= np.exp(-(r4/R)**2)
+    wi[...] = np.exp(-(r_up/R)**2)
+    wi -= np.exp(-(r_down/R)**2)
 elif exp_type == ET.VerticalTwoVortices:
-    vor[...] = np.exp(-(r3/R)**2)
-    vor += np.exp(-(r4/R)**2)
+    wi[...] = np.exp(-(r_up/R)**2)
+    wi += np.exp(-(r_down/R)**2)
 else:
     raise ValueError("No experiment defined for type " + repr(exp_type))
 
 # Optionally add noise
-#noise = np.random.normal(size=vor.shape) * grid.msk
-# TODO: check if these two lines are necessary
-##noise -= grid.domain_integration(noise) * grid.msk / grid.area
-##grid.fill_halo(noise)
-#vor += 1e-3 * noise
+# for vor in [wi, wj, wk]:
+#     noise = np.random.normal(size=vor.shape) * grid.msk
+#     # TODO: check if these two lines are necessary
+#     # noise -= grid.domain_integration(noise) * grid.msk / grid.area
+#     # grid.fill_halo(noise)
+#     vor += 1e-3 * noise
+
+# Set-up the velocity field
+nyles.invert_vorticity()  # TODO: implement this method
 
 # Start the simulation
 nyles.run()

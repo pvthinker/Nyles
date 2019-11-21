3be8c521c8188e49e376c699ecd1214fb89cd7fa
Markus Reinert <markus-reinert@web.de>
2019-11-21 19:29:09 +0100

    Improve automatic determination of the timestep
    Also add some example code to test it.

--- core/nyles.py
+++ core/nyles.py
@@ -24,7 +24,7 @@ class Nyles(object) :
         *private :
             - initiate(param) : loads the desired model and initiates the IO
             defines tend and cfl from the param
-            - compute_dt()
+            - compute_dt(): calculate the timestep
         *public :
             - run() : main loop. Iterates the model forward and saves the state
             in the history file
@@ -64,32 +64,98 @@ class Nyles(object) :
         self.IO.finalize(self.model.state, t, n)
 
     def compute_dt(self):
-        """Calculate the timestep as function of velocity and grid size.
+        """Calculate timestep dt from contravariant velocity U and cfl.
 
-        This function returns the fixed value self.dt0 if self.auto_dt
-        is False or if the velocity is everywhere zero.  Otherwise it
-        calculates the current timestep dt using self.cfl, the size of
-        the grid and the maximal velocity.  The formula (in x) is:
-            dt = cfl * dx^2 / u
-        Note that dx is squared, because u is the covariant velocity,
-        which has dimensions of L^2/T."""
+        The fixed value self.dt0 is returned if and only if self.auto_dt
+        is False or the velocity is zero everywhere (this is an
+        extremely rare case, but must be included to avoid division by
+        zero).  Otherwise, the timestep is calculated as
+            dt = cfl / max(|U|) .
+        Note that the "dx" is hidden in the contravariant velocity U,
+        which has units of 1/T.  In the formula, |U| denotes the norm of
+        the contravariant velocity vector.
+        """
         if self.auto_dt:
-            # Compute maximal velocities
-            u_max = np.max(np.abs(self.state.u["i"].view()))
-            v_max = np.max(np.abs(self.state.u["j"].view()))
-            w_max = np.max(np.abs(self.state.u["k"].view()))
-            # If all velocities are zero, take the standard timestep
-            if u_max == 0.0 and v_max == 0.0 and w_max == 0.0:
+            # Get U, V, and W in the same orientation
+            U_object = self.model.state.U["i"]
+            U = U_object.view()
+            V = self.model.state.U["j"].viewlike(U_object)
+            W = self.model.state.U["k"].viewlike(U_object)
+            # Since the sqrt-function is strictly monotonically
+            # increasing, the order of sqrt and max can be exchanged.
+            # This way, it is only necessary to calculate the square
+            # root of a single value, which is faster.
+            #
+            # One can speed up the calculation by using instead
+            # np.max([np.max(np.abs(U)), np.max(np.abs(V)), np.max(np.abs(W))])
+            # to approximate U_max.  However, this gives only a lower
+            # bound that can differ from the actual value by a factor of
+            # up to sqrt(3) = 1.73.  So with this approximation, the
+            # timestep dt is chosen too big, which could lead to an
+            # unstable integration if cfl is not adjusted accordingly.
+            U_max = np.sqrt(np.max(U**2 + V**2 + W**2))
+            # Note: the if-statement cannot be replaced by try-except,
+            # because U_max is a numpy-float which throws a warning
+            # instead of an error in case of division by zero.
+            if U_max == 0.0:
                 return self.dt0
-            # Otherwise compute dt with cfl, the grid size, and the velocities
-            dt_x = self.cfl * self.grid.dx2 / u_max
-            dt_y = self.cfl * self.grid.dy2 / v_max
-            dt_z = self.cfl * self.grid.dz2 / w_max
-            # Return the smallest non-zero value of dt
-            return np.nanmin([
-                dt_x if dt_x else np.nan,
-                dt_y if dt_y else np.nan,
-                dt_z if dt_z else np.nan,
-            ])
+            else:
+                return self.cfl / U_max
         else:
             return self.dt0
+
+
+if __name__ == "__main__":
+    import topology as topo
+
+    procs = [1, 1, 1]
+    topo.topology = "closed"
+    myrank = 0
+    loc = topo.rank2loc(myrank, procs)
+    neighbours = topo.get_neighbours(loc, procs)
+
+    param = {
+        # IO parameters
+        "datadir": "~/data/Nyles",
+        "expname": "test_exp",
+        "timestep_history": 1.0,
+        "variables_in_history": "all",
+        "mode": "overwrite",
+        # Grid parameters
+        "Lx": 1.0,
+        "Ly": 1.0,
+        "Lz": 1.0,
+        "nx": 32,
+        "ny": 32,
+        "nz": 16,
+        "nh": 0,
+        # Other parameters
+        "neighbours": neighbours,
+        "modelname": "LES",
+        "timestepping": "LFAM3",
+        "tend": 10.0,
+        "auto_dt": True,
+        "cfl": 1.0,
+        "dt": 0.2,
+    }
+
+    nyles = Nyles(param)
+    U = nyles.model.state.U["i"].view()
+    V = nyles.model.state.U["j"].view()
+    W = nyles.model.state.U["k"].view()
+    if param["auto_dt"]:
+        print("Case 1: U = 0, V = 0, W = 0")
+        print("    dt is", nyles.compute_dt())
+        print("should be", param["dt"])
+        U += 1
+        print("Case 2: U = 1, V = 0, W = 0")
+        print("    dt is", nyles.compute_dt())
+        print("should be", param["cfl"] / np.sqrt(1))
+        V += 1
+        print("Case 3: U = 1, V = 1, W = 0")
+        print("    dt is", nyles.compute_dt())
+        print("should be", param["cfl"] / np.sqrt(2))
+        W += 1
+        print("Case 4: U = 1, V = 1, W = 1")
+        print("    dt is", nyles.compute_dt())
+        print("should be", param["cfl"] / np.sqrt(3))

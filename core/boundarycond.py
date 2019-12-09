eb688e6915a2a9f12fbe6b15da5f978e9d2bdca7
pvthinker <roullet@univ-brest.fr>
2019-12-09 18:21:12 +0100

    Add missing boundarycond.py
    oups

---
+++ core/boundarycond.py
@@ -0,0 +1,50 @@
+
+
+def apply_bc_on_velocity(state, ngbs):
+    """ apply no-flow BC on left walls, if any """
+    if (-1, 0, 0) in ngbs.keys():
+        pass
+    else:
+        w = state.u['k'].view('k')
+        w[:, :, 0] = 0.
+
+    if (0, -1, 0) in ngbs.keys():
+        pass
+    else:
+        v = state.u['j'].view('j')
+        v[:, :, 0] = 0.
+        
+    if (0, 0, -1) in ngbs.keys():
+        pass
+    else:
+        u = state.u['i'].view('i')
+        u[:, :, 0] = 0.
+
+        
+def apply_bc_on_vorticity(state, ngbs):
+    """ apply free-slip BC on left walls, if any """
+
+    if (-1, 0, 0) in ngbs.keys():
+        pass
+    else:
+        wj = state.vor['j'].view('k')
+        wj[:, :, 0] = 0.
+        wi = state.vor['i'].view('k')
+        wi[:, :, 0] = 0.
+
+    if (0, -1, 0) in ngbs.keys():
+        pass
+    else:
+        wk = state.vor['k'].view('j')
+        wk[:, :, 0] = 0.
+        wi = state.vor['i'].view('j')
+        wi[:, :, 0] = 0.
+
+        
+    if (0, 0, -1) in ngbs.keys():
+        pass
+    else:
+        wk = state.vor['k'].view('i')
+        wk[:, :, 0] = 0.
+        wj = state.vor['j'].view('i')
+        wj[:, :, 0] = 0.

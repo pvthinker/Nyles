de8a0da3951798b0e25f82a67ddbff224fed00d8
pvthinker <roullet@univ-brest.fr>
2020-05-15 11:29:02 +0200

    Make IOsubdomains more general
    - can read any variable, 1D or 4D (meaning also time and coordinates)
    - allows more general indexing, like b[-1] instead of b[-1,:,:,:]
    - better naming of the variables (shape in particular)
    - the print of a variable is improved

--- tools/IOsudomains.py
+++ tools/IOsudomains.py
@@ -6,6 +6,7 @@ from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 import os
 
+
 class Variable(object):
     def __init__(self, nctemplate, varname, debug=False):
         """
@@ -29,39 +30,68 @@ class Variable(object):
         >>> v = Variable(nctemplate,"b")
         >>> b = v[:, 4, :, -1]
 
+        The method also allows for b=v[-1] or b=b[-1,0]
+        that return respectively a 3D and a 2D array
+
         """
-        nt, shape, procs = self.get_infos(nctemplate % 0)
-        self.nt = nt
-        self.shape = shape # space dimension
-        self.procs = procs
-        self.varname = varname
         self.nctemplate = nctemplate
+        self.varname = varname
         self.debug = debug
 
-    def get_infos(self, ncfile):
-        paramlist = ["nx", "ny", "nz",
-                     "npx", "npy", "npz", "include_halo"]
-        param = {}
-        with Dataset(ncfile) as nc:            
-            for p in paramlist:
-                param[p] = nc.getncattr(p)
-            nt = len(nc.dimensions["t"])
-        print("include_halo: ", param["include_halo"])
-        shape = (param["nz"], param["ny"], param["nx"])
-        procs = (param["npz"], param["npy"], param["npx"])
-        print("shape of local subdomain: ", shape)
-        print("number of frames        : ", nt)
-        print("domain partition        : ", procs)
-        return nt, shape, procs
+        self.check_sanity(varname)
+
+        dimensions, shape, localshape, procs, nprocs = self.get_dimensions(
+            nctemplate % 0)
+
+        self.dimensions = dimensions
+        self.shape = shape
+        self.localshape = localshape
+        self.procs = procs
+        self.nprocs = nprocs
+
+    def check_sanity(self, varname):
+        with Dataset(self.nctemplate % 0) as nc:
+            var = nc.variables
+        varlist = ""
+        for v in var:
+            varlist += str(v)+", "
+        msg = "varname should be in ["+varlist[:-1]+"]"
+        assert varname in var, msg
+
+    def get_dimensions(self, ncfile):
+        with Dataset(ncfile) as nc:
+            var = nc.variables[self.varname]
+            dimensions = var.dimensions
+
+            procs = {}
+            nprocs = 1
+            for d in "xyz":
+                np = nc.getncattr("np"+d)
+                procs[d] = np
+                nprocs *= np
+
+            localshape = []
+            shape = []
+
+            for d in dimensions:
+                n = len(nc.dimensions[d])
+                localshape += [n]
+                if d in "xyz":
+                    n *= procs[d]
+                shape += [n]
+        return dimensions, tuple(shape), tuple(localshape), procs, nprocs
 
     def __repr__(self):
-        msg = ["subdomain dimensions: %r" % (self.dimensions,)]
-        msg += ["variable name: %s" % self.name]
+        msg = ["template    : %s" % self.nctemplate]
+        msg += ["variable    : %s" % self.varname]
+        msg += ["dimensions  : " + str(self.dimensions)]
+        msg += ["global shape: " + str(self.shape)]
+        msg += ["partition   : " + str(self.procs)]
         return "\n".join(msg)
 
     def __getitem__(self, elem):
         """ __getitem__ is a special Python method that allows to access an
-object with bracket and indexing. See
+        object with bracket and indexing. See
 
         https://docs.python.org/2.7/reference/datamodel.html#special-method-names
 
@@ -75,32 +105,65 @@ object with bracket and indexing. See
 
         """
         gloshape = []
+        dimensions = self.dimensions
         shape = self.shape
+        localshape = self.localshape
         procs = self.procs
-        nprocs = np.prod(procs)
+        nprocs = self.nprocs
+        ndims = len(self.dimensions)
+
+        if type(elem) in [int, slice]:
+            elem = [elem]
+        else:
+            elem = list(elem)
+
+        nelem = len(elem)
+
+        # complete missing dimensions with full slices
+        # this means that b[-1] stands for b[-1, :, :, :]
+        if nelem < ndims:
+            elem += [slice(None)]*(ndims-nelem)
 
         if self.debug:
             print("*"*60)
             print("elem:", elem)
 
         s = slice(None)
-        slices = [s, s, s, s]
+        slices = [s]*ndims
         for k, e in enumerate(elem):
-            if k == 0: # time
+            d = dimensions[k]
+            if d == "t":  # time
                 if type(e) is slice:
-                    gloshape += [self.nt]
+                    # <= time is necessary the first dimension
+                    gloshape += [shape[0]]
                 else:
                     slices[k] = e
             else:
-                kk = k-1
                 if type(e) is slice:
-                    gloshape += [shape[kk]*procs[kk]]
+                    gloshape += [shape[k]]
                 elif type(e) is int:
                     if self.debug:
-                        print("cut in direction %i at %i" % (kk, e))
+                        print("cut in direction %s at %i" % (d, e))
                     slices[k] = e
-                    k0 = kk
-                    e0 = e//shape[kk]
+                    k0 = k
+                    e0 = e//shape[k]
+
+        if ndims == 1:
+            d = dimensions[0]
+            npx = procs["x"]
+            npy = procs["y"]
+            npz = procs["z"]
+            if d == "t":
+                procs_to_scan = [0]
+            elif d == "x":
+                procs_to_scan = range(0, npx)
+            elif d == "y":
+                procs_to_scan = range(0, npx*npy, npx)
+            elif d == "z":
+                procs_to_scan = range(0, npx*npy*npz, npx*npy)
+
+        else:
+            procs_to_scan = range(nprocs)
 
         if self.debug:
             print("global returned variable slice is %r" % (gloshape,))
@@ -109,68 +172,70 @@ object with bracket and indexing. See
         # allocate the returned array
         data = np.zeros(gloshape)
 
-        # loop trough all ranks
+        # loop trough subdomains
         nfreads = 0
-        for rank in range(nprocs):
+        for rank in procs_to_scan:
             loc = set_loc(rank, procs)
-
             # is there any data to read from this rank ?
             # yes, if the hyperslab runs through that rank
-            if True:
-                time = slices[0]
-                localelem = [time]
-                if type(time) is slice:
-                    gloelem = [time]
+
+            skip = False
+            gloelem = []
+            localelem = []
+            for k, e in enumerate(elem):
+                d = dimensions[k]
+                if d == "t":  # time
+                    if type(e) is slice:
+                        gloelem += [s]
+                        localelem += [s]
+                    else:
+                        localelem += [e]
+
                 else:
-                    gloelem = []
-                skip = False
-                for k, e in enumerate(elem[1:]):
-                    i0 = loc[k]*shape[k]
+                    i0 = loc[d]*localshape[k]
                     if type(e) is slice:
                         localelem += [e]
-                        gloelem += [slice(i0, i0+shape[k])]
+                        gloelem += [slice(i0, i0+localshape[k])]
                     elif type(e) is int:
                         if e >= 0:
-                            i1 = e-loc[k]*shape[k]
+                            i1 = e-loc[d]*localshape[k]
                         else:
-                            i1 = (shape[k]*procs[k]+e)-loc[k]*shape[k]
-                        if (i1 >= 0) and (i1 < shape[k]):
+                            i1 = (shape[k]+e)-loc[d]*localshape[k]
+                        if (i1 >= 0) and (i1 < localshape[k]):
                             localelem += [i1]
                         else:
                             skip = True
 
-                #print("rank %i / local elem is %r" % (rank, gloelem,))
-                if skip:
-                    pass
-                else:
-                    ncfile = self.nctemplate % rank
-                    if self.debug:
-                        print("gloelem  :", gloelem)
-                        print("localelem:", localelem, "rank=",rank)
-                    with Dataset(ncfile, "r") as nc:
-                        b = nc["b"][tuple(localelem)]
-                        data[tuple(gloelem)] = b
-                        nfreads += 1
-            else:
+            if skip:
                 # no data to read
                 pass
+
+            else:
+                ncfile = self.nctemplate % rank
+                if self.debug:
+                    print("gloelem  :", gloelem)
+                    print("localelem:", localelem, "rank=", rank)
+
+                # this is where we read the subdomain file and
+                # store the result in data, at the good place
+                # the good place is controlled by "gloelem"
+                with Dataset(ncfile, "r") as nc:
+                    data[tuple(gloelem)] = nc[self.varname][tuple(localelem)]
+                    nfreads += 1
 
         if self.debug:
             print("to complete this slice, I read %i netcdf files" % nfreads)
 
         return data
 
-def set_rank(loc):
-    """
-    convert subdomain location (k,j,i) into rank
-    """
-    k, j, i = loc
-    rank = i+procs[0]*(j+procs[1]*k)
-    return rank
-
 
 def set_loc(rank, procs):
-    loc = [rank // (procs[2]*procs[1]),
-           (rank // procs[2]) % procs[1],
-           rank % procs[2]]
+    """how subdomains are organized
+
+    loc[d] is the subdomain coordinate in direction d
+
+    """
+    loc = {"z": rank // (procs["x"]*procs["y"]),
+           "y": (rank // procs["x"]) % procs["y"],
+           "x": rank % procs["x"]}
     return loc
